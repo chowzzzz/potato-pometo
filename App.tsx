178b313a6f8570363832bcddd8b84776e4f7ac3c
MOKXF <mokxf16@gmail.com>
2021-06-15 16:08:47 +0800

    clean up navigation code

--- App.tsx
+++ App.tsx
@@ -4,68 +4,19 @@ import React from "react";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import MainPage from "./screens/MainPage";
-import SignUp from "./screens/SignUp";
-import TabOneScreen from "./screens/TabOneScreen";
-import Game from "./screens/Game";
-
-const Stack = createStackNavigator();
-const RootStack = createStackNavigator();
-
-function StackScreen() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="MainPage"
-                component={MainPage}
-            />
-            <Stack.Screen name="SignUp"
-                component={SignUp}
-                options={{
-                    title: 'Sign Up',
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen name="TabOneScreen"
-                component={TabOneScreen}
-                options={{
-                    title: 'TabOneScreen',
-                    headerShown: false,
-                }}
-            />
-            <Stack.Screen name="Game"
-                component={Game}
-                options={{
-                    title: 'Game',
-                    headerShown: false,
-                }}
-            />
-        </Stack.Navigator>
-    )
-}
-
 export default function App() {
-    const isLoadingComplete = useCachedResources();
-    const colorScheme = useColorScheme();
+	const isLoadingComplete = useCachedResources();
+	const colorScheme = useColorScheme();
 
-    if (!isLoadingComplete) {
-        return null;
-    } else {
-        return (
-            <SafeAreaProvider>
-                {/* <Navigation colorScheme={colorScheme} /> */}
-                <NavigationContainer>
-                    <RootStack.Navigator mode="modal"
-                        headerMode="none"
-                    >
-                        <RootStack.Screen name="MainPage"
-                            component={StackScreen} />
-                    </RootStack.Navigator>
-                </NavigationContainer>
-            </SafeAreaProvider>
-        );
-    }
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+		return (
+			<SafeAreaProvider>
+				<Navigation colorScheme={colorScheme} />
+			</SafeAreaProvider>
+		);
+	}
 }
