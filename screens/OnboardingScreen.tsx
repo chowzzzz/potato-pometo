5f53800a131811a872cff9296d8335ed2577bcef
MOKXF <mokxf16@gmail.com>
2021-06-15 17:09:15 +0800

    add illustrations and update onboarding screen

--- screens/OnboardingScreen.tsx
+++ screens/OnboardingScreen.tsx
@@ -1,16 +1,47 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
 
 const OnboardingScreen = ({ navigation }) => {
 	console.log(navigation);
 	return (
-		<View style={styles.container}>
-			<Text>Onboarding Screen</Text>
-			<Button
-				title="Click Here"
-				onPress={() => navigation.navigate("Login")}
-			/>
-		</View>
+		<Onboarding
+			pages={[
+				{
+					backgroundColor: "#fff",
+					image: (
+						<Image
+							source={require("../assets/illustrations/ginger-cat-work-from-home.png")}
+						/>
+					),
+					title: "Tired of working from home all day?",
+					subtitle:
+						"Pometo is your virtual team building mobile application that keeps you happy and entertained at anytime of the day!"
+				},
+				{
+					backgroundColor: "#fff",
+					image: (
+						<Image
+							source={require("../assets/illustrations/ginger-cat-718.png")}
+						/>
+					),
+					title: "What is a team without team bonding?",
+					subtitle:
+						"With a social feed, your team can bond together by having fun and keeping one another updated!"
+				},
+				{
+					backgroundColor: "#fff",
+					image: (
+						<Image
+							source={require("../assets/illustrations/ginger-cat-739.png")}
+						/>
+					),
+					title: "Work hard, but also play hard!",
+					subtitle:
+						"Play mini games with one another in the team to get to know each other better and see who gets the highest scores!"
+				}
+			]}
+		/>
 	);
 };
 
